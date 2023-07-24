5ee22f47f1141b5e218051a68e6c538e5b27ccb9
srinivasj1987 <132161799+srinivasj1987@users.noreply.github.com>
2023-07-24 19:54:41 -0400

    adding files

---
+++ Crowdfunding_sql_code.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/KjGojW
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" integer   NOT NULL,
+    "contact_id" integer   NOT NULL,
+    "company_name" varchar(300)   NOT NULL,
+    "description" varchar(300)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(50)   NOT NULL,
+    "backers_count" integer   NOT NULL,
+    "country" varchar(50)   NOT NULL,
+    "currency" varchar(10)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar   NOT NULL,
+    "subcategory_ids" varchar   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar   NOT NULL,
+    "category" varchar   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "sub_category" (
+    "subcategory_ids" varchar   NOT NULL,
+    "sub_category" varchar   NOT NULL,
+    CONSTRAINT "pk_sub_category" PRIMARY KEY (
+        "subcategory_ids"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" integer   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "email" varchar   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_ids" FOREIGN KEY("subcategory_ids")
+REFERENCES "sub_category" ("subcategory_ids");
+
